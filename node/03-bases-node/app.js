18f1e6d81c209e1c0444c62d198ce86b8302d8c5
isirichard <isi.12ichard@gmail.com>
2019-03-11 16:44:19 -0500

    crear

--- node/03-bases-node/app.js
+++ node/03-bases-node/app.js
@@ -7,23 +7,48 @@ const argv = require('yargs')
         limite: {
             alias: 'l',
             default: 10
-        }
+        },
+
+    })
+    .command('crear', 'Genera un archivo con la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        },
+
     })
     .help()
     .argv;
 
-const { crearArchivo } = require('./multiplicar/multiplicar')
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar')
     //let base = 5;
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+
+        break;
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archico creado: ${archivo}`))
+            .catch(e => console.log(e));
+
+        break;
+    default:
+        console.log('Comando no reconocido');
+
+}
+//console.log(argv);
+
 
 //console.log(process.argv);
-let argv2 = process.argv;
-
-console.log('Limite', argv.limite);
+//let argv2 = process.argv;
+//console.log('Limite', argv.limite);
 
 // let parametro = argv[2];
 // let base = parametro.split('=')[1];
-
-
-// crearArchivo(base)
-//     .then(archivo => console.log(`Archico creado: ${archivo}`))
-//     .catch(e => console.log(e));
--- node/03-bases-node/multiplicar/multiplicar.js
+++ node/03-bases-node/multiplicar/multiplicar.js
@@ -2,32 +2,40 @@
 const fs = require('fs'); //propia de node nativos
 //const fs = require('express');//paquete que se instala no nativos
 //const fs = require('./fs'); //archivos creamos en el proyecto
-let crearArchivo = (base) => {
+let listarTabla = (base, limite = 10) => {
+    let data = '';
+    for (let i = 1; i <= limite; i++) {
+        //data += `${base} * ${i} = ${base * i}\n`;
+        console.log(`${base} * ${i} = ${base * i}`);
+
+    }
+}
+
+let crearArchivo = (base, limite = 10) => {
     return new Promise((resolve, reject) => {
         if (!Number(base)) {
             reject(`El valor introducido ${base} no es un número`);
             return;
         }
 
-
-
         let data = '';
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= limite; i++) {
             data += `${base} * ${i} = ${base * i}\n`;
             //console.log(`${base} * ${i} = ${base * i}`);
 
         }
 
 
-        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+        fs.writeFile(`tablas/tabla-${base}.txt-al-${limite}`, data, (err) => {
             if (err) reject(err)
             else
-                resolve(`tabla-${base}.txt`)
+                resolve(`tabla-${base}-al-${limite}.txt`)
                 //console.log(`El archivo tabla-${base}.txt ha sido creado`);
         });
     });
 }
 
 module.exports = {
-    crearArchivo
+    crearArchivo,
+    listarTabla
 }
