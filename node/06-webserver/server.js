d546741d52df5c40bb9cf41e5b35f3959f2a6816
isirichard <isi.12ichard@gmail.com>
2019-03-23 18:05:53 -0500

    webserver

---
+++ node/06-webserver/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'))
+
+// app.get('/', (req, res) => {
+//     //res.send('Hola Mundo');
+//     let salida = {
+//         nombre: 'fernando',
+//         edad: 32,
+//         url: req.url
+//     };
+//     res.send(salida);
+
+// });
+
+// app.get('/data', (req, res) => {
+//     res.send('Hola data');
+
+// });
+
+app.listen(3000, () => {
+    console.log('Escuchando peticiones en el puerto 3000');
+});
