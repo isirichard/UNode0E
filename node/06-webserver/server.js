3b71161f1d44c2a3b989dcf7c559b59795dd44d2
isirichard <isi.12ichard@gmail.com>
2019-03-25 00:07:45 -0500

    heroku

--- node/06-webserver/server.js
+++ node/06-webserver/server.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const app = express();
+//importar paquetes
+const hbs = require('hbs');
+//herencia
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'))
 
 //Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+
+
+
+
 app.set('view engine', 'hbs');
 
-// app.get('/', (req, res) => {
-//     //res.send('Hola Mundo');
-//     let salida = {
-//         nombre: 'fernando',
-//         edad: 32,
-//         url: req.url
-//     };
-//     res.send(salida);
+//helpers
 
-// });
 
-// app.get('/data', (req, res) => {
-//     res.send('Hola data');
+app.get('/', (req, res) => {
+    //res.send('Hola Mundo');
 
-// });
+    res.render('home', {
+        nombre: 'Fernando'
+    });
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones en el puerto 3000');
+});
+
+
+
+app.get('/about', (req, res) => {
+    res.render('about');
+
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
 });
