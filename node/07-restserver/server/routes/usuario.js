88559a10aea89578738ed1d433e5ad5711ac8d68
isirichard <isi.12ichard@gmail.com>
2019-04-09 12:24:04 -0500

    udemy

---
+++ node/07-restserver/server/routes/usuario.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const app = express();
+const Usuario = require('../models/usuario');
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario Local!!!')
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
+        //usuarioDB.password = null;
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+    });
+
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    // delete body.google;
+    // delete body.password;
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err: err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+    })
+
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario')
+});
+
+module.exports = app;
