93208aec830b247752b0152cb2ecdb44d44e8a1c
isirichard <isi.12ichard@gmail.com>
2019-04-09 21:24:50 -0500

    variable de enterno heroku

--- node/07-restserver/server/config/config.js
+++ node/07-restserver/server/config/config.js
@@ -14,7 +14,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://isinick:GMQjGNx54RnVHYlD@cluster0-7ewho.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
+    //urlDB = 'mongodb+srv://isinick:GMQjGNx54RnVHYlD@cluster0-7ewho.mongodb.net/cafe';
 }
 
 process.env.URLDB = urlDB;
